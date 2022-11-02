c15011c841771db4414b621feb7e115624195eb3
prangonsafayet <prangonsafayet@gmail.com>
2022-11-02 17:18:41 +0100

    form submit done

--- frontend/src/shared/components/FormElements/Input.js
+++ frontend/src/shared/components/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 
 import { validate } from "../../util/validators";
 
@@ -28,6 +28,14 @@ const Input = (props) => {
     isTouched: false,
     isValid: false,
   });
+
+  const {id, onInput} = props;
+  const {value, isValid} =inputState;
+
+  useEffect(()=>{
+    onInput(id, value, isValid)
+  },[id, value, isValid, onInput]);
+
   const changeHandler = (event) => {
     dispatch({
       type: "CHANGE",
@@ -35,11 +43,13 @@ const Input = (props) => {
       validators: props.validators,
     });
   };
+
   const touchHandler = () => {
     dispatch({
       type: "TOUCH",
     });
   };
+
   const element =
     props.element === "input" ? (
       <input
